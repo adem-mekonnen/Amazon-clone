08a5bb4ba8620365dd4f5907e2c6bf9f3e9f4550
adem-mekonnen <adem.mekonin@wku.edu.et>
2024-03-07 01:01:43 +0300

    axios added

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //baseURL: "http://127.0.0.1:5001/clone-29741/us-central1/api",
-  baseURL: " https://amazon-api-deploy-4djd.onrender.com",
+  baseURL: " https://amazon-api-deploy-2.onrender.com",
 });
 
 export { axiosInstance };
