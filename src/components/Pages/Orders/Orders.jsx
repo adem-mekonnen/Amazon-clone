29452a9dc8aeb1d3cf91e8a8083c5f505628c609
adem-mekonnen <adem.mekonin@wku.edu.et>
2024-03-06 11:54:23 +0300

    Building orders page UI

--- src/components/Pages/Orders/Orders.jsx
+++ src/components/Pages/Orders/Orders.jsx
@@ -1,5 +1,59 @@
+import { useContext, useState, useEffect } from "react";
+import classes from "./Orders.module.css";
+import Layout from "../../Layout/Layout";
+import { DataContext } from "../../DataProvider/DataProvider";
+import ProductCard from "../../Product/ProductCard";
+import { db } from "../../../Utility/firebase";
 const Orders = () => {
-  return <div>Orders</div>;
+  const [{ user }, dispatch] = useContext(DataContext);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) => {
+          console.log("ppppppp", snapshot);
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          );
+        });
+    } else {
+      setOrders([]);
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <section className={classes.container}>
+        <div className={classes.orders__container}>
+          <h2>Your Orders</h2>
+          {orders?.length == 0 && (
+            <div style={{ padding: "20px" }}>you don't have orders yet.</div>
+          )}
+          {/* ordered items */}
+          <div>
+            {orders?.map((eachOrder, i) => {
+              return (
+                <div key={i}>
+                  <hr />
+                  <p>Order ID: {eachOrder?.id}</p>
+                  {eachOrder?.data?.basket?.map((order) => (
+                    <ProductCard flex={true} product={order} key={order.id} />
+                  ))}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Orders;
