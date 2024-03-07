6eb251f582476585790e06b77bcaae23b90463dd
adem-mekonnen <adem.mekonin@wku.edu.et>
2024-03-07 17:57:26 +0300

    add netify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "http://127.0.0.1:5001/clone-29741/us-central1/api",
-  baseURL: " https://amazon-api-deploys-1.onrender.com",
+  baseURL: "http://127.0.0.1:5001/clone-29741/us-central1/api",
+  //baseURL: " https://amazon-api-deploys-1.onrender.com",
 });
 
 export { axiosInstance };
